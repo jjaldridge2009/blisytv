71ae934d2c3dadcb42c738931c0c6be9bedf73f7
notblisy <50887637+notblisy@users.noreply.github.com>
2020-04-23 20:02:55 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,34 @@ using namespace std;
 
 int main()
 {
-
-
 int intv, outtv, targf, hitf, tvin, tvprint, outprint, hitframe, adjtv, adjtv2, donttype;
-float remainder, remaintv, adjout;
+long double remainder, remaintv, adjout, nds, gba, ms, msout, mstvout, bigtvout, bigout, lowerbetternumber, fasttv;
+
+gba = 59.7275;
+nds = 59.6555;
+std::cout << "Type the Number that Corresponds to your game console:\n";
+std::cout << "[1] Nintendo DS\n"
+          << "[2] Gameboy Advance\n";
+
+std::string input;
+std::getline(std::cin, input);
+
+while(input != "1" && input != "2") {
+    std::cout << "Invalid!\n\n";
+
+std::cout << "Type the Number that Corresponds to your game console:\n";
+std::cout << "[1] Nintendo DS\n"
+          << "[2] Gameboy Advance\n";
+    std::getline(std::cin, input);
+}
+
+if (input == "1") {
+   ms = nds;
+} else if (input == "2") {
+   ms = gba;
+}
+
+
 cout <<"Enter Target Frame: ";
 cin >> intv;
 
@@ -19,26 +43,50 @@ tvin = intv - outtv;
 tvprint = tvin /313;
 remainder= tvin % 313;
 outprint =  remainder + outtv;
+mstvout = tvprint / ms * 1000;
+msout = outprint / ms * 1000;
 cout << "Frames in TeachyTV: ";
 cout <<tvprint;
 cout << "      ";
 cout << "Frames Out of TeachyTV: ";
 cout <<outprint<<endl;
-
-
+cout << "Total Frames: ";
+cout <<tvprint + outprint<<endl;
+cout.precision(0);
+cout.setf(ios_base::fixed);
+cout << "Milliseconds in TeachyTV: ";
+cout <<mstvout;
+cout << "      ";
+cout << "Milliseconds Out of TeachyTV: ";
+cout << msout<<endl;
+cout << "Total Milliseconds: ";
+cout << msout + mstvout<<endl;
 
 cout <<"Enter Frame hit: ";
 cin >> hitframe;
 
 adjtv = intv - hitframe;
-adjtv2= adjtv /313;
-adjout= adjtv % 313;
+adjtv2= adjtv / 313;
+adjout = adjtv % 313;
+if( adjout > 156){
+adjout = 313 - adjout;
+adjtv2--;
+} else if ( adjout < -156){
+adjout = 313 + adjout;
+adjtv2--;
+
+}
 
 cout <<"Adjust TeachyTV by: ";
 cout <<adjtv2;
 cout << "      ";
 cout <<"Adjust out of TeachyTV frames by: ";
-cout <<adjout;
+cout <<adjout<<endl;
+cout <<"Adjust TeachyTV MS by: ";
+cout <<adjtv2 / ms * 1000;
+cout << "      ";
+cout << "Adjust out of TeachyTV Ms by: ";
+cout <<adjout / ms *1000;
 cin>> donttype;
     return 0;
 }
